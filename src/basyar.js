9ade44b893585098361badf44fc00bdf0a2fc3a0
mbasyar <muhammadkhoirulb@gmail.com>
2023-02-07 12:34:10 +0700

    commit pertama

---
+++ src/basyar.js
@@ -0,0 +1,15 @@
+
+
+function Basyar() {
+    return (
+      <div className="Basyar">
+            <p>
+              my name is basyar
+            </p>
+            <h1>sedang belajar react</h1>
+      
+      </div>
+    );
+  }
+
+  export default Basyar;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Basyar from './basyar';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <Basyar />
   </React.StrictMode>
 );
 
