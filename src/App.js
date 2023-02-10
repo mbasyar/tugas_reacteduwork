f103f95631ed2d48b048a00d8eba3f5b16bf0787
mbasyar <muhammadkhoirulb@gmail.com>
2023-02-10 08:38:05 +0700

    menghapus logo react

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello World</h1>
-      <img src='/gambar.png' style={{width: '500px', borderRadius: '15px', margin: 'auto'}} alt='gambar' /> 
+      <img src='/gambar.png' style={{width: '500px', borderRadius: '15px', margin: 'auto', border: '1px solid black'}} alt='gambar' /> 
       <br />
-        <img src={logo} className="App-logo" alt="logo" />
-        <h2> ini project react pertama saya </h2>
+        <div className='content'>
+          <h2> ini project react pertama saya </h2>
+        </div>
     </div>
   );
 }
