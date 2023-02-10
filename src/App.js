cd57edd1ad09fe7dc40fffa1cb2b077bbe3b3033
mbasyar <muhammadkhoirulb@gmail.com>
2023-02-10 09:12:49 +0700

    memodif isi app.js dan app.css

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-      <img src='/gambar.png' style={{width: '500px', borderRadius: '15px', margin: 'auto', border: '1px solid black'}} alt='gambar' /> 
-      <br />
-        <div className='content'>
-          <h2> ini project react pertama saya </h2>
-        </div>
+    <div className='container' >
+      <div className="App">
+        <h1>Eduwork ReactJS </h1>
+        <img src='/favicon.ico' style={{width: '200px', borderRadius: '15px', margin: 'auto'}} alt='gambar' /> 
+          <div className='content'>
+            <p>penggunaan Javascript yang sederhana dalam pembuatan website, ReactJS banyak menjadi pilihan bagi para developer.</p>
+          </div>
+      </div>
     </div>
   );
 }
